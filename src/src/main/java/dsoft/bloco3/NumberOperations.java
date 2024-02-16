50f16f2fccfa0436312bb15327908be4bc63ea25
rueloParente <1231858@isep.ipp.pt>
2024-02-16 12:29:39 +0000

    Exercise 6.2 - first Palindromic in range + tests

--- src/src/main/java/dsoft/bloco3/NumberOperations.java
+++ src/src/main/java/dsoft/bloco3/NumberOperations.java
@@ -2,7 +2,7 @@ package dsoft.bloco3;
 
 public class NumberOperations {
     //Verify if number is capicua
-    public static boolean isNumberCapicua (int num){
+    public static boolean isNumberPalindromic(int num){
         int[] digits = Digits.numberToArrayDigits(num);
         int[] invertedDigits = Digits.invertArrayOrder(digits);
         int invertedNum = Digits.arrayOfDigitsToNumber(invertedDigits);
@@ -16,5 +16,12 @@ public class NumberOperations {
             result += (int) Math.pow(digit, 3);
         }
         return result == num;
+    }
+    //Return first capicua number in a given interval
+    public static int firstPalindromicInRange(int min, int max){
+        for (int i=min; i<=max; i++){
+            if (NumberOperations.isNumberPalindromic(i)) return i;
+        }
+        return -1; // as error code, no capicua in range
     }
 }
--- src/src/test/java/dsoft/bloco3/NumberOperationsTest.java
+++ src/src/test/java/dsoft/bloco3/NumberOperationsTest.java
@@ -6,20 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberOperationsTest {
     @Test
-    void shouldReturnTrueIfNumberIsCapicua() {
+    void shouldReturnTrueIfNumberIsPalindromic() {
         //Arrange
         boolean expected = true;
         //Act
-        boolean result = NumberOperations.isNumberCapicua(12321);
+        boolean result = NumberOperations.isNumberPalindromic(12321);
         //Assert
         assertEquals(expected, result);
     }
     @Test
-    void shouldReturnFalseIfNumberIsNotCapicua() {
+    void shouldReturnFalseIfNumberIsNotPalindromic() {
         //Arrange
         boolean expected = false;
         //Act
-        boolean result = NumberOperations.isNumberCapicua(12345);
+        boolean result = NumberOperations.isNumberPalindromic(12345);
         //Assert
         assertEquals(expected, result);
     }
@@ -39,6 +39,35 @@ class NumberOperationsTest {
         boolean expected = false;
         //Act
         boolean result = NumberOperations.isNumberAmstrong(123);
+        //Assert
+        assertEquals(expected, result);
+    }
+    //First capicua in range
+    @Test
+    void shouldReturnFirstPalindromicInRange() {
+        //Arrange
+        int expected = 101;
+        //Act
+        int result = NumberOperations.firstPalindromicInRange(100, 200);
+        //Assert
+        assertEquals(expected, result);
+    }
+    @Test
+    void shouldReturnMinusOneIfNoPalindromicInRange() {
+        //Arrange
+        int expected = -1;
+        //Act
+        int result = NumberOperations.firstPalindromicInRange(12, 21);
+        //Assert
+        assertEquals(expected, result);
+    }
+    @Test
+    //palindromic has to be a real number, negative numbers are not palindromic
+    void shouldReturnFirstPalindromicInRangeNegative() {
+        //Arrange
+        int expected = 121;
+        //Act
+        int result = NumberOperations.firstPalindromicInRange(120, 130);
         //Assert
         assertEquals(expected, result);
     }
